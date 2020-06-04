55774ecfb96cabe0bb646bcc7f77b051f6e69e75
Ryan <Ryan@Ryan-PC>
2020-06-04 15:14:23 +0800

    PlaneGame

---
+++ src/cn/ryan/game/Constant.java
@@ -0,0 +1,11 @@
+﻿package cn.ryan.game;
+
+/**
+ ** 窗口长和宽
+ * @author Ryan
+ *
+ */
+public class Constant {
+	public static final int GAME_WIDTH = 500;
+	public static final  int  GAME_HEIGHT = 500;
+}
